fb197fa95d9a768ed22ef74d355b591ae18396e2
AM-Maidana <amanda.maidana@outlook.com>
2025-03-21 16:57:52 -0400

    Introdução ao aspnet e atividade de curriculo

---
+++ Aula1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace teste
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Builder = WebApplication.CreateBuilder(args);
+            var app = Builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoins =>{
+                endpoins.MapGet("/", async context => {
+                    context.Response.Redirect("/index.html");
+                });
+
+            });
+
+            app.Run();
+        }
+
+        // public static IHostBuilder CreateHostBuilder(string[] args)
+        // => { // define o metodo que é responsqvel para criar o host de aplicações
+        //     Host.CreateDefaultBuilder(args) // Cria oo host com configuração padrão do abiente
+
+        //     .ConfigureWebHostDefaults(WebHostBuilder)
+        
+        // }
+    }
+}
